cf9b08d3155e8ea548f2f452e6a262817be96903
yangchen29 <yangchen29@outlook.com>
2019-07-30 09:54:05 +0800

    更改匹配接口的返回值

--- Views/websocket/room.go
+++ Views/websocket/room.go
@@ -3,6 +3,7 @@ package websocket
 import (
 	. "PrintHalf/Models"
 	"github.com/googollee/go-socket.io"
+	"log"
 )
 
 var matching []socketio.Conn
@@ -50,16 +51,28 @@ func match(s1, s2 socketio.Conn) {
 			UserId1: s1.Context().(int),
 			UserId2: s2.Context().(int),
 		}
+		user1 := UserModel{Id: picture.UserId1}
+		user2 := UserModel{Id: picture.UserId2}
+		_, err := db.Get(user1)
+		_, err = db.Get(user2)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
 		db.Insert(picture)
 		matching = matching[1:]
 		s1.Emit("match", jsonify{
-			"message":         "匹配成功",
-			"another_user_id": s2.Context().(int),
+			"message": "匹配成功",
+			"data": jsonify{
+				"another_user_name": user2.Name,
+			},
 			// 其他再加
 		})
 		s2.Emit("match", jsonify{
-			"message":         "匹配成功",
-			"another_user_id": s1.Context().(int),
+			"message": "匹配成功",
+			"data": jsonify{
+				"another_user_name": user1.Name,
+			},
 			// 其他再加
 		})
 	}
